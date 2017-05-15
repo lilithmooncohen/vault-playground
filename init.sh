4895b03c92cda772ecfd29cb3d2386de422b08a9
Ryan C Koch <ryankoch@gmail.com>
2017-05-15 14:44:57 -0600

    cleaning up

--- init.sh
+++ init.sh
@@ -15,13 +15,6 @@ VAULT_UNSEAL_KEY_4=$(grep "Unseal Key 4" init-secret.txt | awk '{print $4}')
 VAULT_UNSEAL_KEY_5=$(grep "Unseal Key 5" init-secret.txt | awk '{print $4}')
 VAULT_INITIAL_ROOT_KEY=$(grep "Initial Root Token" init-secret.txt | awk '{print $4}')
 
-# echo "Unseal Key 1: ${VAULT_UNSEAL_KEY_1}"
-# echo "Unseal Key 2: ${VAULT_UNSEAL_KEY_2}"
-# echo "Unseal Key 3: ${VAULT_UNSEAL_KEY_3}"
-# echo "Unseal Key 4: ${VAULT_UNSEAL_KEY_4}"
-# echo "Unseal Key 5: ${VAULT_UNSEAL_KEY_5}"
-# echo "Initial Root Token : ${VAULT_INITIAL_ROOT_KEY}"
-
 echo "Unsealing Vault:"
 docker-compose exec vault vault unseal ${VAULT_UNSEAL_KEY_1}
 docker-compose exec vault vault unseal ${VAULT_UNSEAL_KEY_2}
